a32d79bf7756c1a9475d8e428d47f64238d9d525
bomberman1990 <son.levuthai@gmail.com>
2015-12-14 09:41:33 +0700

    get feature

--- app.js
+++ app.js
@@ -23,4 +23,17 @@ router.get('/api/v1/todos', TodoCtroller.get);
 router.put('/api/v1/todos/:todo_id', TodoCtroller.put);
 router.delete('/api/v1/todos/:todo_id', TodoCtroller.delete);
 
+app.use(function (request, response) {
+    response.status(404).json({
+        message: 'Oops! You may get lost!',
+        code: 'E_NOTFOUND'
+    });
+});
+
+app.use(function (error, request, response, next) {
+    response.status(404).json({
+        message: error.message,
+        code: error.code
+    });
+});
 module.exports = app;
--- routes/todo.js
+++ routes/todo.js
@@ -1,15 +1,133 @@
-exports.post = function(request, response) {
+var tedious = require('tedious');
+
+exports.post = function(req, res) {
+
+    var results = [];
+
+    // Grab data from http request
+    var data = {text: req.body.text, complete: false};
+
+
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+            done();
+            console.log(err);
+            return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Insert Data
+        client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+
+
+    });
+};
+
+exports.get = function(req, res) {
+    var results = [];
+    var query = new tedious.Request("SELECT * FROM items ORDER BY id ASC", function (error) {
+        if (error) {
+            return res.status(500).json({ success: false, data: err});
+        }
+    });
+    query.on('row', function(row) {
+        results.push(row);
+    });
+    query.on('done', function() {
+        done();
+        return res.json(results);
+    });
+    req.app.get('db').execSql(sqlRequest);
+};
+
+exports.put = function(req, res) {
+
+    var results = [];
+
+    // Grab data from the URL parameters
+    var id = req.params.todo_id;
+
+    // Grab data from http request
+    var data = {text: req.body.text, complete: req.body.complete};
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+            done();
+            console.log(err);
+            return res.status(500).send(json({ success: false, data: err}));
+        }
+
+        // SQL Query > Update Data
+        client.query("UPDATE items SET text=($1), complete=($2) WHERE id=($3)", [data.text, data.complete, id]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
 
 };
 
-exports.get = function(request, response) {
-    response.json({foo: 'bar'});
-};
+exports.delete = function(req, res) {
 
-exports.put = function(request, response) {
+    var results = [];
 
-};
+    // Grab data from the URL parameters
+    var id = req.params.todo_id;
 
-exports.delete = function(request, response) {
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+            done();
+            console.log(err);
+            return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Delete Data
+        client.query("DELETE FROM items WHERE id=($1)", [id]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
 
 };
