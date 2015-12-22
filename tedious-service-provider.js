c04311b4859b0a8e0ea745fbc2ea606f34acac7e
Long Pham <longpham91@gmail.com>
2015-12-22 18:37:57 -0500

    fix

--- tedious-service-provider.js
+++ tedious-service-provider.js
@@ -1,6 +1,5 @@
 var tedious = require('tedious');
 var config  = require('./config/tedious-connection');
-var ConnectionPool = require('tedious-connection-pool');
 
 module.exports = function (request, response, next) {
     var connection = new tedious.Connection(config);
